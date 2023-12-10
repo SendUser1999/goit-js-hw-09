db1ace1d82801d13f0702d43a59f5a5da460ecad
SendUser1999 <anastasiya.sendeckaya89@gmail.com>
2023-12-10 19:16:19 +0100

    changes

---
+++ js/01-color-switcher.js
@@ -0,0 +1,29 @@
+const bodyEl = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+const DELAY = 1000;
+let timerId = null;
+
+startButton.addEventListener('click', onStartBtnClick);
+stopButton.addEventListener('click', onStopBtnClick);
+stopButton.disabled = true;
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    bodyEl.style.background = getRandomHexColor();
+  }, DELAY);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  startButton.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
---
+++ js/03-promises.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('form.form');
+const delay = document.querySelector('input[name=delay]');
+const step = document.querySelector('input[name=delay]');
+const amount = document.querySelector('input[name=amount');
+
+
+form.addEventListener('click', onCreatePromiseClick);
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onCreatePromiseClick(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget.elements;
+  let inputDelay = Number(delay.value);
+  let inputStep = Number(step.value);
+  let inputAmount = Number(amount.value);
+
+  for (let i = 1; i <= inputAmount; i += 1) {
+    createPromise(i, inputDelay)
+      .then(({ position, delay }) => {
+        console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`Rejected promise ${position} in ${delay}ms`);
+      });
+    inputDelay += inputStep;
+  }
+}
+
